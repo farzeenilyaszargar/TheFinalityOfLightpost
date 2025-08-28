14dfa90c170e07555d414a9f9efb413183425392
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2025-08-28 15:06:25 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="">
       <p>Yo</p>
+      <Header/>
+
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header()
+{
+    return(
+        <div className="h-20  w-screen flex justify-center my-grey">
+
+            <div className="w-5/6 border-l border-r flex text-5xl justify-center items-center dark-grey-b h-20">
+                <p className="--font-logo text-white">LIGHTPOST</p>
+            </div>
+        </div>
+    );
+}
